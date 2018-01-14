ede3825d395c762684cbe2ed3a9fb2fcdcb5424e
Obura Tongoi <obura@tongoi.com>
2018-01-14 08:15:46 -0800

    Add support for L5.5
    Change Swift_Mime_Message to Swift_Mime_SimpleMessage
    Add "guzzlehttp/guzzle" to dependancies

--- src/ElasticTransport.php
+++ src/ElasticTransport.php
@@ -4,11 +4,11 @@ namespace Chocoholics\LaravelElasticEmail;
 
 use GuzzleHttp\ClientInterface;
 use Illuminate\Mail\Transport\Transport;
-use Swift_Mime_Message;
+use Swift_Mime_SimpleMessage;
 
 class ElasticTransport extends Transport
 {
-	
+
 	/**
      * Guzzle client instance.
      *
@@ -49,17 +49,17 @@ class ElasticTransport extends Transport
     public function __construct(ClientInterface $client, $key, $account)
     {
     	$this->client = $client;
-        $this->key = $key;
-        $this->account = $account;
+      $this->key = $key;
+      $this->account = $account;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function send(Swift_Mime_Message $message, &$failedRecipients = null)
+    public function send(Swift_Mime_SimpleMessage $message, &$failedRecipients = null)
     {
         $this->beforeSendPerformed($message);
-       
+
         $data = [
             'api_key' => $this->key,
             'account' => $this->account,
@@ -79,20 +79,20 @@ class ElasticTransport extends Transport
         $result = $this->client->post($this->url, [
         	'form_params' => $data
 		]);
-        
+
         return $result;
     }
 
     /**
      * Get the plain text part.
      *
-     * @param  \Swift_Mime_Message $message
+     * @param  \Swift_Mime_SimpleMessage $message
      * @return text|null
      */
-    protected function getText(Swift_Mime_Message $message)
+    protected function getText(Swift_Mime_SimpleMessage $message)
     {
         $text = null;
-        
+
         foreach($message->getChildren() as $child)
 		{
 			if($child->getContentType() == 'text/plain')
@@ -100,27 +100,27 @@ class ElasticTransport extends Transport
 				$text = $child->getBody();
 			}
 		}
-        
+
         return $text;
     }
-	
+
 	/**
-	 * @param \Swift_Mime_Message $message
+	 * @param \Swift_Mime_SimpleMessage $message
 	 *
 	 * @return array
 	 */
-    protected function getFromAddress(Swift_Mime_Message $message)
+    protected function getFromAddress(Swift_Mime_SimpleMessage $message)
 	{
 		return [
 			'email' => array_keys($message->getFrom())[0],
 			'name' => array_values($message->getFrom())[0],
 		];
 	}
-	
-	protected function getEmailAddresses(Swift_Mime_Message $message, $method = 'getTo')
+
+	protected function getEmailAddresses(Swift_Mime_SimpleMessage $message, $method = 'getTo')
 	{
 		$data = call_user_func([$message, $method]);
-		
+
 		if(is_array($data))
 		{
 			return implode(',', array_keys($data));
